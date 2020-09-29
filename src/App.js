5efd0667b1b3cab31b005087c3e5af7e6b9d32d5
YashSardhara <60999988+YashSardhara@users.noreply.github.com>
2020-09-29 17:47:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./Components/layout/Navbar";
+import Dashboard from "./Components/dashboard/Dashboard";
+import ProjectDetails from "./Components/projects/projectDetails";
+import SignIn from "./Components/auth/SignedIn";
+import SignUp from "./Components/auth/Signedup";
+import CreateProject from "./Components/projects/createProject";
+import SocialMedia from "./Components/layout/SocialMedia";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Dashboard />
+          </Route>
+          <Route path="/project/:id">
+            <ProjectDetails />
+          </Route>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/create_project">
+            <CreateProject />
+          </Route>
+
+          <Route path="/social">
+            <SocialMedia />
+          </Route>
+        </Switch>
+      </React.Fragment>
+    </Router>
   );
 }
 
